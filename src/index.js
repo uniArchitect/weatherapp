3a86f265e36de9767c59fa413363639c021a68d2
uniArchitect <hancho93@gmail.com>
2022-08-27 19:08:25 -0400

    FEAT: add input and update button to update API location

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
+// GLOBAL VARIABLE
+const BODY = document.querySelector('body');
+const CONTAINER = document.createElement('div');
+const LOCATION_INPUT = document.createElement('input');
+const LOCATION_UPDATE_BTN = document.createElement('button');
+
+LOCATION_UPDATE_BTN.innerHTML = 'Update';
+
+BODY.appendChild(CONTAINER);
+CONTAINER.appendChild(LOCATION_INPUT);
+CONTAINER.appendChild(LOCATION_UPDATE_BTN);
 // Geocoding API
 // Promise Version
 // refreshAPI = () => {
@@ -17,11 +28,11 @@
 // };
 
 // Async Version
-async function currentWeather() {
+async function currentWeather(location) {
   try {
     const geocodeResponse = await fetch(
-      "http://api.openweathermap.org/geo/1.0/direct?q=New York,USA&limit=5&appid=1228246814ef93d1b972cc316a42abeb",
-      { mode: "cors" }
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location},USA&limit=5&appid=1228246814ef93d1b972cc316a42abeb`,
+      { mode: 'cors' },
     );
     const geocode = await geocodeResponse.json();
     const geocodeLat = geocode[0].lat;
@@ -29,14 +40,16 @@ async function currentWeather() {
 
     const weatherResponse = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${geocodeLat}&lon=${geocodeLon}&appid=1228246814ef93d1b972cc316a42abeb`,
-      { mode: "cors" }
+      { mode: 'cors' },
     );
     const weatherAPI = await weatherResponse.json();
     console.log(weatherAPI);
-
   } catch (err) {
     console.log(err);
   }
 }
 
-currentWeather();
+LOCATION_UPDATE_BTN.addEventListener('click', () => {
+  let location = LOCATION_INPUT.value;
+  currentWeather(location);
+})
