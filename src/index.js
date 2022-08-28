4492dd111382eec1ae5adcef8574180166cea96a
uniArchitect <hancho93@gmail.com>
2022-08-27 20:20:55 -0400

    STYLES: add flex and grid layout styles to div elements

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import currentWeather from "./weatherAPI";
 const BODY = document.querySelector("body");
 const CONTAINER_DIV = document.createElement("div");
 const USER_INPUT_CONTAIN_DIV = document.createElement("div");
+const WEATHER_CARD_CONTAIN_DIV = document.createElement("div");
 const LOCATION_INPUT = document.createElement("input");
 const LOCATION_UPDATE_BTN = document.createElement("button");
 
@@ -16,10 +17,11 @@ LOCATION_INPUT.classList.add("location-input");
 LOCATION_UPDATE_BTN.classList.add("location-submit");
 LOCATION_UPDATE_BTN.innerHTML = "Update";
 
+WEATHER_CARD_CONTAIN_DIV.classList.add("weather-container");
+
 BODY.appendChild(CONTAINER_DIV);
-CONTAINER_DIV.appendChild(USER_INPUT_CONTAIN_DIV);
-USER_INPUT_CONTAIN_DIV.appendChild(LOCATION_INPUT);
-USER_INPUT_CONTAIN_DIV.appendChild(LOCATION_UPDATE_BTN);
+CONTAINER_DIV.append(USER_INPUT_CONTAIN_DIV, WEATHER_CARD_CONTAIN_DIV);
+USER_INPUT_CONTAIN_DIV.append(LOCATION_INPUT, LOCATION_UPDATE_BTN);
 
 LOCATION_UPDATE_BTN.addEventListener("click", () => {
   let location = LOCATION_INPUT.value;
