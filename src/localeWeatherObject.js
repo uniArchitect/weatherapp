c25f446d2592d8bb5d68ff8400e4f773801f1657
uniArchitect <hancho93@gmail.com>
2022-08-30 22:23:37 -0400

    PROGRESS: add Math.round() function to numbers to round to whole numbers

--- src/localeWeatherObject.js
+++ src/localeWeatherObject.js
@@ -23,8 +23,8 @@ export default class weatherObject {
   // Convert to Fahrenheit = 1.8(K-273)+32
 
   static convertCelsius = (weatherObject) => {
-    let cTemperature = weatherObject.temperature - 273.15;
-    let cFeel = weatherObject.feel - 273.15;
+    let cTemperature = Math.round(weatherObject.temperature - 273.15);
+    let cFeel = Math.round(weatherObject.feel - 273.15);
 
     let cArray = [cTemperature, cFeel];
 
@@ -32,8 +32,8 @@ export default class weatherObject {
   };
 
   static convertFahrenheit = (weatherObject) => {
-    let fTemperature = 1.8 * (weatherObject.temperature - 273) + 32;
-    let fFeel = 1.8 * (weatherObject.feel - 273) + 32;
+    let fTemperature = Math.round(1.8 * (weatherObject.temperature - 273) + 32);
+    let fFeel = Math.round(1.8 * (weatherObject.feel - 273) + 32);
 
     let fArray = [fTemperature, fFeel];
 
@@ -44,7 +44,7 @@ export default class weatherObject {
 
   // Wind speed defined as mph or kph?
 static convertMPH = (weatherObject) => {
-    let mph = 2.24 * (weatherObject.wind);
+    let mph = Math.round(2.24 * (weatherObject.wind));
 
     return mph;
 }
@@ -64,9 +64,9 @@ static convertMPH = (weatherObject) => {
     <p>Location: ${weatherObject.location}</p>
     <p>Description: ${weatherObject.description}</p>
     <p>Temperature: ${fahrenheit}</p>
-    <p>Feels like: ${fahrenheitFeel}</p>
+    <p>Feels Like: ${fahrenheitFeel}</p>
     <p>Humidity: ${weatherObject.humidity}%</p>
-    <p>Wind speed: ${windSpeedConverted}</p>
+    <p>Wind Speed: ${windSpeedConverted} MPH</p>
     `;
 
     // Change background image based on temperature ranges (<=32, snowy, 33=<, sunny)
