b0c4cb209e31edf63ba46266d103a6ab4440d600
uniArchitect <hancho93@gmail.com>
2022-09-06 21:10:11 -0400

    FEAT: add button element and event listener to switch between F and C unit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
 import './styles.css';
-import { currentWeatherImperial, currentWeatherMetric, currentWeatherBothUnits } from './weatherAPI';
+import { currentWeatherImperial, currentWeatherMetric } from './weatherAPI';
 import WeatherUI from './WeatherUI';
 
 /* This is creating the elements that will be used in the app. */
@@ -13,19 +13,22 @@ const USER_INPUT_CONTAIN_DIV = document.createElement('div');
 const WEATHER_CARD_CONTAIN_DIV = document.createElement('div');
 const LOCATION_INPUT = document.createElement('input');
 const LOCATION_UPDATE_BTN = document.createElement('button');
+const UNIT_TOGGLE_BUTTON = document.createElement('button');
 
 // SET UP - WEB APP ELEMENTS
 CONTAINER_DIV.classList.add('main-container');
 USER_INPUT_CONTAIN_DIV.classList.add('input-container');
 LOCATION_INPUT.classList.add('location-input');
 LOCATION_UPDATE_BTN.classList.add('location-submit');
-LOCATION_UPDATE_BTN.innerHTML = 'Update';
+LOCATION_UPDATE_BTN.innerHTML = 'Search';
+UNIT_TOGGLE_BUTTON.classList.add('unit-button');
+UNIT_TOGGLE_BUTTON.innerHTML = '°F';
 
 WEATHER_CARD_CONTAIN_DIV.classList.add('weather-container');
 
 BODY.appendChild(CONTAINER_DIV);
 CONTAINER_DIV.append(USER_INPUT_CONTAIN_DIV, WEATHER_CARD_CONTAIN_DIV);
-USER_INPUT_CONTAIN_DIV.append(LOCATION_INPUT, LOCATION_UPDATE_BTN);
+USER_INPUT_CONTAIN_DIV.append(LOCATION_INPUT, LOCATION_UPDATE_BTN, UNIT_TOGGLE_BUTTON);
 
 // Event - Update location
 LOCATION_UPDATE_BTN.addEventListener('click', () => {
@@ -45,6 +48,18 @@ WEATHER_CARD_CONTAIN_DIV.addEventListener('click', (e) => {
 
   WeatherUI.changeWeatherUnit(e.target);
 });
+
+// Event: Change Has Read Status
+UNIT_TOGGLE_BUTTON.addEventListener('click', (e) => {
+  let currentUnit = e.target;
+
+  // Specify if statement to target elements with class 'book-status'
+  if(currentUnit.textContent.includes('°F')) {
+    currentUnit.innerHTML = '°C';
+  } else if(currentUnit.textContent.includes('°C')){
+    currentUnit.textContent = '°F';
+  }
+})
 
 // eslint-disable-next-line import/prefer-default-export
 export { WEATHER_CARD_CONTAIN_DIV };
